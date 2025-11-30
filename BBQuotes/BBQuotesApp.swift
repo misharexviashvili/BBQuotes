f24dfaf751d230b2b2da82858ff02bd5f89fdb4e
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2025-11-30 13:52:55 +0400

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Credo on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
