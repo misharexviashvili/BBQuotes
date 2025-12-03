43404ad432302eafc7bae92d4d0e8f633abb2293
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2025-12-03 22:32:37 +0400

    fetch service providers

--- BBQuotes/FetchService.swift
+++ BBQuotes/FetchService.swift
@@ -26,4 +26,42 @@ struct FetchService {
         
         return quote
     }
+    
+    func fetchCharacter (_ name : String) async throws -> Char {
+        let characterURL = baseURL.appending(path: "characters")
+        let fetchURL = characterURL.appending(queryItems: [URLQueryItem(name: "name", value: name)])
+        
+        let (data, resposne) = try await URLSession.shared.data(from: fetchURL)
+        guard let response = resposne as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let characters = try decoder.decode([Char].self, from: data)
+        
+        
+        return characters[0]
+    }
+    
+    func fetchDeath (for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "deaths")
+        
+        let (data, resposne) = try await URLSession.shared.data(from: fetchURL)
+        guard let response = resposne as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deaths = try decoder.decode([Death].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        return nil
+    }
 }
